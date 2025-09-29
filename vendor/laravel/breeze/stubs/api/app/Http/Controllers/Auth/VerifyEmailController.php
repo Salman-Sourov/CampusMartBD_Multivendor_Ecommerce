2d3cf2154741639bc06cefbc41c6666a213f06ed
Salman-Sourov <salman.sourov40@gmail.com>
2025-09-30 01:26:12 +0600

    full

---
+++ vendor/laravel/breeze/stubs/api/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\RedirectResponse;
+use App\Models\Product_category;
+
+class VerifyEmailController extends Controller
+{
+    public function showVerifyForm()
+    {
+        $categories = Product_category::with('translations', 'hasChild')
+            ->where('level', '1')
+            ->where('status', 'active')
+            ->get();
+
+        return view('auth.verify', compact('categories'));
+    }
+
+    public function __invoke(EmailVerificationRequest $request)
+    {
+        $user = $request->user();
+
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->route('index')->with('message', 'Your email is already verified!');
+        }
+
+        $user->markEmailAsVerified();
+        event(new Verified($user));
+
+        return redirect()->route('index')->with('message', 'Email verified successfully!');
+    }
+}
